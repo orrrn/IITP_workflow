24c7aaa0f2b65aa20069d9dba49e0ece7111687f
Jung In Su <jis0727@ajou.ac.kr>
2023-10-12 10:50:52 +0900

    v0.7(mac)

--- SIRP.py
+++ SIRP.py
@@ -11,14 +11,37 @@ import openpyxl
 # pbtable_non = pbtable.dropna(how="any")
 # playbook_list = pbtable_non['playbook existence'].unique()  # extract mitre ics attack name list
 
-filename = './dataset/ics-attack-playbook-list1-v13.1.xlsx' # mitre ics attack list
-playbook_list_tmp = pd.read_excel(filename)
-pb_table = playbook_list_tmp[['ID', 'playbook name', 'playbook existence']]
-pb_table_non = pb_table.dropna(how="any")
-playbook_name_list = pb_table_non['playbook name'].unique()  # extract mitre ics attack name list
-playbook_exist_list = pb_table_non['playbook existence'].unique()
-playbook_list = list(zip(playbook_name_list, playbook_exist_list))
-# print(playbook_list)
+def sirp_playbook_data():
+    filename = './dataset/ics-attack-playbook-list1-v13.1.xlsx' # mitre ics attack list
+    playbook_list_tmp = pd.read_excel(filename)
+    pb_table = playbook_list_tmp[['ID', 'playbook name', 'playbook existence']]
+    pb_table_non = pb_table.dropna(how="any")
+    playbook_ID_list = pb_table_non['ID'].unique()
+    playbook_name_list = pb_table_non['playbook name'].unique()  # extract mitre ics attack name list
+    playbook_exist_list = pb_table_non['playbook existence'].unique()
+    playbook_list = [[ID, playbook_name, playbook_existence] for ID, playbook_name, playbook_existence in zip(playbook_ID_list, playbook_name_list, playbook_exist_list)]
+
+    # playbook_list = list(zip(playbook_name_list, playbook_exist_list))
+
+    return playbook_list
+
+# def tip_attack_data():
+#     filename = './dataset/ics-attack-v13.1.xlsx'  #mitre ics attack list
+#     attack_list_tmp = pd.read_excel(filename, sheet_name="techniques")
+#     att_table = attack_list_tmp[['ID', 'name', 'description', 'created', 'last modified', 'version', 'tactics']]
+#     att_table_non = att_table.dropna(how="any")
+#     attack_id_list = att_table_non['ID'].unique()
+#     attack_name_list = att_table_non['name'].unique()                      #extract mitre ics attack name list
+#     # attack_description_list = att_table_non['description'].unique()
+#     attack_created_list = att_table_non['created'].unique()
+#     attack_last_modified_list = att_table_non['last modified'].unique()
+#     attack_version_list = att_table_non['version'].unique()
+#     attack_tactics_list = att_table_non['tactics'].unique()        
+#     # attack_list = [[ID, name, created, last_modifed, version, tactics] for ID, name, created, last_modifed, version, tactics in zip(attack_id_list, attack_name_list, attack_created_list, attack_last_modified_list, attack_version_list, attack_tactics_list)]
+#     attack_list = [[ID, name] for ID, name in zip(attack_id_list, attack_name_list)]
+#     # print("data related security threat was collected")
+#     # collect data related security threat
+#     return attack_list
 
 
 
@@ -35,14 +58,34 @@ def sirp_match_playbook(case):
 
     # maching playbook lists using for(iterate)
 
-    for i in playbook_list:
-        if case[1] == i:
-            case[3] = 1 # playbook exists and set classification available
+    for i in range(len(sirp_playbook_data())):
+        if case[1] == sirp_playbook_data()[i][1]:
+            case[4] = 1 # playbook exists and set classification available
             break
         else:
-            case[3] = 0 # playbook does not exists and set classification unavailable
+            case[4] = 0 # playbook does not exists and set classification unavailable
 
     # maching playbook lists
+
+    for i in range(len(sirp_playbook_data())):     # is attack_list check
+        if case[1] == sirp_playbook_data()[i][1]:
+            # print(case[1])
+            # print(tip_attack_data()[i][1])
+            case[3] = 1             # attack classificable check (1 is able)
+            SIRP.sirp_match_playbook(case)
+            break
+        else:
+            case[3] = 0    # attack unclassificable check (0 is unable)
+
+            # print("this is the length of list")
+            # print(case[1])
+            # print("--------------------------------------------")
+            # print(tip_attack_data()[i][1])
+            # print("this is not classificable attack")
+            # print("this " + case[1] + "is not classified as" + i)
+
+    print("TIP Process is finished")
+
 
     return case
 
--- TIP.py
+++ TIP.py
@@ -16,21 +16,26 @@ def tip_process(case):
     else:
         case[2] == 0 # this is not attack
 
+    case = ["IT", "Denial of Service", 1, None, 5, None, None, None, None]
+
     print("Classification Accident Process is going..")
 
-    for i in tip_attack_data():     # is attack_list check
-        if case[1] == i:
-            print("this is classificable attack")
+    for i in range(len(tip_attack_data())):     # is attack_list check
+        if case[1] == tip_attack_data()[i][1]:
+            # print(case[1])
+            # print(tip_attack_data()[i][1])
             case[3] = 1             # attack classificable check (1 is able)
             SIRP.sirp_match_playbook(case)
             break
         else:
-            print("this is not classificable attack")
-            case[3] = 0
+            case[3] = 0    # attack unclassificable check (0 is unable)
 
-    Classification_accident.classification_accident_process(case)
-
-    case = ["IT", "Exploitation for Privilege Escalation=", 1, None, 5, None, None, None]
+            # print("this is the length of list")
+            # print(case[1])
+            # print("--------------------------------------------")
+            # print(tip_attack_data()[i][1])
+            # print("this is not classificable attack")
+            # print("this " + case[1] + "is not classified as" + i)
 
     print("TIP Process is finished")
 
@@ -40,12 +45,18 @@ def tip_process(case):
 def tip_attack_data():
     filename = './dataset/ics-attack-v13.1.xlsx'  #mitre ics attack list
     attack_list_tmp = pd.read_excel(filename, sheet_name="techniques")
-    att_table = attack_list_tmp[['ID', 'name']]
+    att_table = attack_list_tmp[['ID', 'name', 'description', 'created', 'last modified', 'version', 'tactics']]
     att_table_non = att_table.dropna(how="any")
     attack_id_list = att_table_non['ID'].unique()
-    attack_name_list = att_table_non['name'].unique()                                   #extract mitre ics attack name list
-    attack_list = list(zip(attack_id_list, attack_name_list))
-
-    print("data related security threat was collected")
+    attack_name_list = att_table_non['name'].unique()                      #extract mitre ics attack name list
+    # attack_description_list = att_table_non['description'].unique()
+    attack_created_list = att_table_non['created'].unique()
+    attack_last_modified_list = att_table_non['last modified'].unique()
+    attack_version_list = att_table_non['version'].unique()
+    attack_tactics_list = att_table_non['tactics'].unique()        
+    # attack_list = [[ID, name, created, last_modifed, version, tactics] for ID, name, created, last_modifed, version, tactics in zip(attack_id_list, attack_name_list, attack_created_list, attack_last_modified_list, attack_version_list, attack_tactics_list)]
+    attack_list = [[ID, name] for ID, name in zip(attack_id_list, attack_name_list)]
+    # print("data related security threat was collected")
     # collect data related security threat
     return attack_list
+
